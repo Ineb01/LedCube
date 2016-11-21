0006f8440ca8212f702c94bb187b09989b2e0b2a
ڪтнємαx 「αuт」 <x u@XMG.fritz.box>
2016-11-21 21:09:14 +0100

    Moved defines to a header - file.

--- LedCube/main.c
+++ LedCube/main.c
@@ -21,32 +21,10 @@
 #include <time.h>
 #include <pthread.h>
 //#include <wiringPi.h>
-#define LEDCUBESIDE 3
-#define LAYER0 6
-#define LAYER1 4
-#define LAYER2 5
 
-#define PILLAR1 10
-#define PILLAR2 13
-#define PILLAR3 2
-#define PILLAR4 11
-#define PILLAR5 12
-#define PILLAR6 0
-#define PILLAR7 14
-#define PILLAR8 3
-#define PILLAR9 7
-
-//#define HIGH 1
-//#define LOW 0
-//#define OUTPUT 777
-//#define INPUT 777
-
-#define TASTE1  1
+#include "define.h"
 
 
-/*
- * 
- */
 void PrintCube();
 void* PrintThread();
 void RandomCube(int probability);
