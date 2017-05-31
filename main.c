eeb6b5bf95887be57eb93d9d2f07cc2ea6ace541
mfaller <mfaller@A101D3.HTLSTEYR>
2017-05-31 11:11:27 +0200

    Adding some info to the file - reading.

--- main.c
+++ main.c
@@ -36,11 +36,44 @@
 #include "console.h"
 #endif
 
-// Marker is used for ....
+// Pattern is used to read a whole ledcube-pattern from the file.
+// Syntax P
+// 1 = HIGH; 0 = LOW
+// A whitespace is needed after every single number.
+#define PATTERN 'P'
+
+// Marker is used to remember a position.
 // Syntax M <x>
-//   x ....
+// x .... Marker Number x
 #define MARKER 'M'
-#define MAX_MARKER  3
+#define MAX_MARKER 3
+
+// Jumper is used to jump back to the Marker with the same index.
+// Syntax J <x>
+#define JUMPER 'J'
+
+// Rotate right is used to rotate the cube 45° to the right.
+// Syntax RR
+#define ROTATE_RIGHT 'R'
+
+// Rotate left is used to rotate the cube 45° to the left.
+// Syntax RL
+#define ROTATE_LEFT 'L'
+
+// Random cube is used to set a random cube with a probability of <x> [percent]
+// for every LED to be on.
+// Syntax RC
+// x [next linie in the file] .... probability for each LED
+#define RANDOM_CUBE 'C'
+
+// Random diode is used to turn on a random LED
+// Syntax RD
+#define RANDOM_DIODE 'D'
+
+// Delay is used to wait after an action.
+// Syntax D
+// x [next linie in the file] .... Delay in milliseconds
+#define DELAY 'D'
 
 #ifdef LEDCUBE_H
 int rgLayerID[LEDCUBESIDE] = {LAYER0, LAYER1, LAYER2};
@@ -63,7 +96,7 @@ int main(int argc, char** argv) {
     pthread_t thread;
     pthread_create(&thread, NULL, THREAD, NULL);
 
-    /*int i, j, k;
+    int i, j, k;
 
     FILE *fp = NULL;
     
@@ -80,11 +113,11 @@ int main(int argc, char** argv) {
             }
             fclose(fp);
         }
-    }*/
-    int i, j, k;
+    }
+    /*int i, j, k;
     signed char input = '0';
     int delay, probability, id;
-    long int marker[3];
+    long int marker[MAX_MARKER];
     int count[3] = {0, 0, 0};
     int control[3] = {0, 0, 0};
     
@@ -101,11 +134,20 @@ int main(int argc, char** argv) {
             fscanf(fp, "%c", &input);
 
             switch (input) {
+                case PATTERN:
+                    for (i = 0; i < LEDCUBESIDE; i++) {
+                        for (j = 0; j < LEDCUBESIDE; j++) {
+                            for (k = 0; k < LEDCUBESIDE; k++) {
+                                fscanf(fp, "%d", &rgLedCube[i][j][k]);
+                            }
+                        }
+                    }
+                    break;
                 case MARKER:
                     fscanf(fp, "%d", &id);
                     marker[id] = ftell(fp);
                     break;
-                case 'J':
+                case JUMPER:
                     fscanf(fp, "%d", &id);
                     if (control[id] == 0) {
                         control[id] = 1;
@@ -121,35 +163,26 @@ int main(int argc, char** argv) {
                 case 'R':
                     fscanf(fp, "%c", &input);
                     switch (input) {
-                        case 'R':
+                        case ROTATE_RIGHT:
                             Rotate45right();
                             break;
-                        case 'C':
+                        case ROTATE_LEFT:
+                            Rotate45left();
+                            break;
+                        case RANDOM_CUBE:
                             fscanf(fp, "%d", &probability);
                             RandomCube(probability);
                             break;
-                        case 'L':
-                            Rotate45left();
-                            break;
-                        case 'D':
+                        case RANDOM_DIODE:
                             RandomLed();
                             break;
                         default:
                             break;
                     }
                     break;
-                case 'D':
+                case DELAY:
                     fscanf(fp, "%d", &delay);
                     usleep(delay * 1000);
-                    break;
-                case 'P':
-                    for (i = 0; i < LEDCUBESIDE; i++) {
-                        for (j = 0; j < LEDCUBESIDE; j++) {
-                            for (k = 0; k < LEDCUBESIDE; k++) {
-                                fscanf(fp, "%d", &rgLedCube[i][j][k]);
-                            }
-                        }
-                    }
                     break;
                 default:
                     break;
@@ -159,6 +192,6 @@ int main(int argc, char** argv) {
 
         fclose(fp);
     }
-
+*/
     return (EXIT_SUCCESS);
 }
